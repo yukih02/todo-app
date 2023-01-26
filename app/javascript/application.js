fbbeb60de3a9cb4d848154dfcd02f964c8b76a16
yukih02 <2yiku31@gmail.com>
2023-01-26 18:20:04 +0900

    一部修正した

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+//= link application.js
 import "@hotwired/turbo-rails"
 import "controllers"
